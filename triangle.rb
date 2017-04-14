267989ebc32823a03640a93ad1329cb2b6da0de9
prism colour <prismcolour@icloud.com>
2017-04-14 01:51:49 +0000

    Skipped this triangle problem

--- triangle.rb
+++ triangle.rb
@@ -14,12 +14,29 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && a == c                
-    :equilateral
+  # Check for exceptions
+  # http://www.peteonsoftware.com/index.php/2009/08/16/ruby-koans/
+  # Wanted to skip this and look at it later - code taken from above
+  # Not really interested in working out a triangle problem 
+  # Hit a break wall and needed to learn/pratice the rest of Ruby
+  
+  # Pete's code for raising exceptions 
+   if (a <= 0 or b <= 0 or c<=0)
+    raise TriangleError
+  end
+  
+  sides = [a, b, c].sort
+  
+  if (sides[0] + sides[1] <= sides[2])
+    raise TriangleError
+  end
+  
+  if a == b && a == c  
+    :equilateral 
   elsif a == b || a == c || b == c   
     :isosceles
   else
-    :scalene                         
+    :scalene
   end
 end
 
